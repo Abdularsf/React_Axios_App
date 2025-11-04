1ff9a84a3c2fb6df0de59fc3823bac0a4f5cb07c
Abdul Rahman <abdularsf147@gmail.com>
2025-11-04 15:55:07 +0530

    Post function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+import { getPost } from "./API/AuthAPI"
+
 export const App = () => {
+
+  const getPostData = async () => {
+    const res = await getPost();
+  }
+
+  useEffect(() => {
+    getPostData();
+  }, [])
+
   return <h1>Hello React Crud Operation</h1>
-  
 }
